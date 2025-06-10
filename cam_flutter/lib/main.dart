8c2382f5724b0a6cd7015f617eca9fee13a038e9
Hendrik Rennefeld <hendrik.rennefeld@gmail.com>
2025-06-10 17:27:19 +0200

    Fix UDP receiver to use ephemeral port

--- cam_flutter/lib/main.dart
+++ cam_flutter/lib/main.dart
@@ -1,50 +1,45 @@
-import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:provider/provider.dart';
+import 'udp_stream_receiver.dart';
 
 void main() {
   runApp(const CamApp());
 }
 
-class CamApp extends StatefulWidget {
+class CamApp extends StatelessWidget {
   const CamApp({super.key});
 
   @override
-  State<CamApp> createState() => _CamAppState();
-}
-
-class _CamAppState extends State<CamApp> {
-  final _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8081'));
-  Uint8List? _frame;
-
-  @override
-  void initState() {
-    super.initState();
-    _channel.stream.listen((data) {
-      setState(() {
-        _frame = data as Uint8List;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Cam Stream')),
-        body: Center(
-          child: _frame == null
-              ? const Text('Waiting for stream...')
-              : Image.memory(_frame!),
+    return ChangeNotifierProvider(
+      create: (_) => UdpStreamReceiver(camIp: '192.168.4.153', camPort: 8080),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: const Text('Cam Stream')),
+          body: const Center(child: _CamView()),
         ),
       ),
     );
   }
+}
+
+class _CamView extends StatelessWidget {
+  const _CamView();
 
   @override
-  void dispose() {
-    _channel.sink.close();
-    super.dispose();
+  Widget build(BuildContext context) {
+    final receiver = context.watch<UdpStreamReceiver>();
+    return ValueListenableBuilder<Uint8List?>(
+      valueListenable: receiver.frame,
+      builder: (context, data, _) {
+        if (data == null) {
+          return const Text('Waiting for stream...');
+        }
+        return Image.memory(data);
+      },
+    );
   }
 }
