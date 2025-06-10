9c3ad0ec5a9c601737d39b12931d9245d6f7ad6b
Hendrik Rennefeld <hendrik.rennefeld@gmail.com>
2025-06-10 17:42:54 +0200

    Add frame processing and UI controls

--- cam_flutter/lib/main.dart
+++ cam_flutter/lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:convert';
 import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+
+import 'frame_processor.dart';
 
 void main() {
   runApp(const CamApp());
@@ -16,15 +18,33 @@ class CamApp extends StatefulWidget {
 
 class _CamAppState extends State<CamApp> {
   final _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8081'));
+  Uint8List? _rawFrame;
   Uint8List? _frame;
+
+  bool _rotate = false;
+  bool _flipH = false;
+  bool _flipV = false;
+  bool _grayscale = false;
 
   @override
   void initState() {
     super.initState();
     _channel.stream.listen((data) {
-      setState(() {
-        _frame = data as Uint8List;
-      });
+      _rawFrame = data as Uint8List;
+      _applyProcessing();
+    });
+  }
+
+  void _applyProcessing() {
+    if (_rawFrame == null) return;
+    final processor = FrameProcessor(
+      rotate90: _rotate,
+      flipH: _flipH,
+      flipV: _flipV,
+      grayscale: _grayscale,
+    );
+    setState(() {
+      _frame = processor.process(_rawFrame!);
     });
   }
 
@@ -33,10 +53,47 @@ class _CamAppState extends State<CamApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Cam Stream')),
-        body: Center(
-          child: _frame == null
-              ? const Text('Waiting for stream...')
-              : Image.memory(_frame!),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ToggleButtons(
+                isSelected: [_rotate, _flipH, _flipV, _grayscale],
+                onPressed: (index) {
+                  setState(() {
+                    switch (index) {
+                      case 0:
+                        _rotate = !_rotate;
+                        break;
+                      case 1:
+                        _flipH = !_flipH;
+                        break;
+                      case 2:
+                        _flipV = !_flipV;
+                        break;
+                      case 3:
+                        _grayscale = !_grayscale;
+                        break;
+                    }
+                  });
+                  _applyProcessing();
+                },
+                children: const [
+                  Icon(Icons.rotate_90_degrees_ccw),
+                  Icon(Icons.swap_horiz),
+                  Icon(Icons.swap_vert),
+                  Icon(Icons.filter_b_and_w),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: _frame == null
+                    ? const Text('Waiting for stream...')
+                    : Image.memory(_frame!),
+              ),
+            ),
+          ],
         ),
       ),
     );
