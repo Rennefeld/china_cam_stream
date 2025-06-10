970ba759d534a69d704f508a2fff560aafba00f0
Hendrik Rennefeld <hendrik.rennefeld@gmail.com>
2025-06-10 17:40:17 +0200

    Add LogService with file logging

--- cam_flutter/lib/main.dart
+++ cam_flutter/lib/main.dart
@@ -2,8 +2,12 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'services/log_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LogService.init();
+  LogService.instance.info('Application started');
   runApp(const CamApp());
 }
 
@@ -15,17 +19,27 @@ class CamApp extends StatefulWidget {
 }
 
 class _CamAppState extends State<CamApp> {
-  final _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8081'));
+  late final WebSocketChannel _channel;
   Uint8List? _frame;
 
   @override
   void initState() {
     super.initState();
-    _channel.stream.listen((data) {
-      setState(() {
-        _frame = data as Uint8List;
-      });
-    });
+    LogService.instance.info('Connecting to stream');
+    _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8081'));
+    _channel.stream.listen(
+      (data) {
+        setState(() {
+          _frame = data as Uint8List;
+        });
+      },
+      onError: (error) {
+        LogService.instance.error('WebSocket error: $error');
+      },
+      onDone: () {
+        LogService.instance.info('WebSocket closed');
+      },
+    );
   }
 
   @override
@@ -44,6 +58,7 @@ class _CamAppState extends State<CamApp> {
 
   @override
   void dispose() {
+    LogService.instance.info('Disposing app');
     _channel.sink.close();
     super.dispose();
   }
